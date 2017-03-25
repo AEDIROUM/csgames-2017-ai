89d4eeb62ff98a09b5421a818d1def3cc6355f2b
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2017-03-24 20:27:51 -0400

    Parse incoming lines and initial state

--- src/client.py
+++ src/client.py
@@ -6,11 +6,22 @@ from twisted.protocols.basic import LineReceiver
 
 from hockey.action import Action
 
+import re
+import numpy as np
+from itertools import product
 
 class HockeyClient(LineReceiver, object):
     def __init__(self, name, debug):
         self.name = name
         self.debug = debug
+
+        # state
+        self.grid = np.zeros(shape=(11, 11))
+        self.edge_taken = np.zeros(shape=(11, 11, 11, 11))
+        self.ball_position = (5, 5)
+        self.goal = False
+
+        self.grid[5, 5] = True
 
     def connectionMade(self):
         self.sendLine(self.name)
@@ -20,15 +31,34 @@ class HockeyClient(LineReceiver, object):
 
     def lineReceived(self, line):
         line = line.decode('UTF-8')
+
         if self.debug:
             print('Server said:', line)
+            print(self.grid)
+
+        match = re.match(r'ball is at \((\d+), (\d+\)) - \d+', line)
+        if match:
+            self.ball_position = int(match.group(1)), int(match.group(2))
+            return
+
+        match = re.match(r'your goal is (\w+) - \d+', line)
+        if match:
+            self.goal = match.group(1)
+            return
+
+        match = re.match(r'\w+ did go (\w+) - \d+', line)
+        if match:
+            direction = match.group(1)
+            return
+
+        if re.match(r'\w+ won a goal was made - \d+', line):
+            return # fin de la partie
+
         if '{} is active player'.format(self.name) in line or 'invalid move' in line:
-            self.play_game()
+            self.sendLine(self.play_game())
 
-    def play_game(self):
-        result = Action.from_number(random.randint(0, 7))
-        self.sendLine(result)
-
+    def play_game():
+        return Action.from_number(random.randint(0, 7))
 
 class ClientFactory(protocol.ClientFactory):
     def __init__(self, name, debug):
@@ -49,7 +79,7 @@ class ClientFactory(protocol.ClientFactory):
         reactor.stop()
 
 
-name = "Bob{}".format(random.randint(0, 999))
+name = "Kek{}".format(random.randint(0, 999))
 
 f = ClientFactory(name, debug=True)
 reactor.connectTCP("localhost", 8023, f)
